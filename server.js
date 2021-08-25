7330cbbe7218549db59114dad7d4ece2db0daea1
Sophanorin <heousopharin168@gmail.com>
2021-08-26 00:37:15 +0700

    start deployment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
