c5105757250a8de56b6584feb42ed612f8dc10c5
Sophanorin <heousopharin168@gmail.com>
2021-09-03 23:14:43 +0700

    done

--- server.js
+++ server.js
@@ -14,12 +14,13 @@ app.use(express.urlencoded({ extended: true }));
  * @param {String} Path
  * @param {Function} require
  */
-app.use("/api/postType", require("./routers/postTypeRouter"));
+
 app.use("/api/post", require("./routers/postRouter"));
 app.use("/api/user", require("./routers/userRouter"));
 app.use("/api/category", require("./routers/categoryRouter"));
 app.use("/api/currency", require("./routers/currencyRouter"));
 app.use("/api/unit", require("./routers/unitRouter"));
+app.use("/api/postType", require("./routers/postTypeRouter"));
 
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
